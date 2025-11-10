d3bb2f47baf0410cd2ae7ae9f7f9d55bf5640be6
Evillina-star <evelinarevina3@gmail.com>
2025-11-10 21:34:54 +0300

    Add files via upload

---
+++ Пр3/TicketApp/TicketApp/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace TicketApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(154, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(420, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " Генератор и проверка счастливого билета";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(284, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 43);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Сгенерировать билет";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(231, 164);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(310, 25);
+            this.label.TabIndex = 5;
+            this.label.Text = "Нажмите на кнопку для генерации";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label2.Location = new System.Drawing.Point(198, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(443, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Счастливый билет_это билет у которого сумма первых трех чисел";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Проверка счастливого билета";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Label labelTicket;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Пр3/TicketApp/TicketApp/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicketApp
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitializeMyComponents();
+        }
+
+
+
+        private void InitializeMyComponents()
+        {
+            this.Text = "Проверка счастливого билета";
+            this.Size = new Size(400, 250);
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            labelTicket = new Label()
+            {
+                Text = "Нажмите кнопку для генерации",
+                Location = new Point(60, 40),
+                AutoSize = true,
+                Font = new Font("Segoe UI", 12)
+            };
+
+            buttonGenerate = new Button()
+            {
+                Text = "Сгенерировать билет",
+                Location = new Point(110, 90),
+                Size = new Size(160, 35)
+            };
+            buttonGenerate.Click += ButtonGenerate_Click;
+
+            labelResult = new Label()
+            {
+                Text = "",
+                Location = new Point(60, 150),
+                AutoSize = true,
+                Font = new Font("Segoe UI", 14, FontStyle.Bold)
+            };
+
+            this.Controls.Add(labelTicket);
+            this.Controls.Add(buttonGenerate);
+            this.Controls.Add(labelResult);
+        }
+
+        private void ButtonGenerate_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(0, 999999);
+            string ticket = number.ToString("D6"); // форматируем до 6 цифр
+            labelTicket.Text = $"Ваш билет: {ticket}";
+
+            int sum1 = (ticket[0] - '0') + (ticket[1] - '0') + (ticket[2] - '0');
+            int sum2 = (ticket[3] - '0') + (ticket[4] - '0') + (ticket[5] - '0');
+
+            if (sum1 == sum2)
+            {
+                labelResult.Text = "Поздравляем! Счастливый билет ";
+                labelResult.ForeColor = Color.Green;
+            }
+            else
+            {
+                labelResult.Text = "Обычный билет ";
+                labelResult.ForeColor = Color.Red;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+        
+            
+        
+    
+
+
+
+
+
+
+
+    
